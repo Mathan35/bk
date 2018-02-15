5ed25b2c945059974e3030edb2597f965b188a82
bmathankumar <36471835+bmathankumar@users.noreply.github.com>
2018-02-15 09:42:03 +0530

    Create largest num.py

---
+++ largest num.py
@@ -0,0 +1,13 @@
+5)find the  largest number between among three numbers
+number1 = 35
+number2 = 43
+number3 = 51
+
+if (number1 > number2) and (number1 > number3):
+ biggest = num1
+elif (number2 > number1) and (number2 > number3):
+ biggest = number2
+else:
+ biggest = number3
+
+print("The biggest number between",number1,",",number2,"and",number3,"is",biggest)
