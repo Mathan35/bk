6564b70c953bf327d4296a8cf4aff6252196c274
bmathankumar <36471835+bmathankumar@users.noreply.github.com>
2018-03-07 17:39:44 +0530

    Create vowelreverse.py

---
+++ vowelreverse.py
@@ -0,0 +1,14 @@
+while True:
+	print("Enter 'x' for exit.")
+	string = input("Enter any string: ")
+	if string == 'x':
+		break
+	else:
+		newstr = string
+		print("\nRemoving vowels from the given string...")
+		vowels = ('a', 'e', 'i', 'o', 'u')
+		for x in string.lower():
+			if x in vowels:
+				newstr = newstr.replace(x,"")
+		print("New string after successfully removing all vowels!")
+		print(newstr[::-1])
